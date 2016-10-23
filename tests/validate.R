b80bc6b22dbe768e776bfe4a30eb3444dd26ebb2
Dirk Eddelbuettel <edd@debian.org>
2016-10-23 09:30:50 -0500

    new (hard) test for utctime
    also tested on win-builder and rhub

--- tests/validate.R
+++ tests/validate.R
@@ -17,3 +17,11 @@ tz <- "Japan"
 output <- format(as.POSIXct(input, tz=tz), tz=tz)
 #stopifnot(format(anytime(input)) == output)
 cat(format(anytime(input)), output, "\n")
+
+
+## when using UTC parse, and output format, we can we assurred
+## a fixed result nomatter where this runs
+tz <- "UTC"
+output <- format(as.POSIXct(input, tz=tz), tz=tz)
+stopifnot(format(utctime(input, tz=tz)) == output)
+cat(format(utctime(input, tz=tz)), output, "\n")
