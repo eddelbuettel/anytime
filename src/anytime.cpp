139686f4411784ad8b3ae0779e63e779778780b7
Dirk Eddelbuettel <edd@debian.org>
2016-11-19 10:46:39 -0600

    more robust string splitting

--- src/anytime.cpp
+++ src/anytime.cpp
@@ -187,10 +187,11 @@ double stringToTime(const std::string s, const bool asUTC=false) {
 // use to do two things:
 //  i)  split yyyymmdd hhmmss[.fff] into date and time parts
 //  ii) for time part, split possible fractional seconds off
-void stringSplitter(/*const*/ std::string & in, const char split,
+void stringSplitter(const std::string & in, const char split,
                     std::string & tok1, std::string& tok2) {
 
-    char *txt = const_cast<char*>(in.c_str());
+    std::string cp = in;        // make another copy to keep input string alone
+    char *txt = const_cast<char*>(cp.c_str());
     tok1 = tok2 = "";
 
     char *token = std::strtok(txt, &split);
@@ -249,9 +250,9 @@ Rcpp::NumericVector convertToTime(const Rcpp::Vector<RTYPE>& sxpvec,
             if (isAtLeastGivenLengthAndAllDigits(one, 8)) {
                 one = one.substr(0, 4) + "-" + one.substr(4, 2) + "-" + one.substr(6,2);
 
-                if ((two.size()==5 || two.size() >= 8) &&          // if we have hh:mm or hh:mm:ss[.ffffff]
-                    !isAtLeastGivenLengthAndAllDigits(two, 6)) {  // and it is not hhmmss
-                    three = "";                         // do nothing, three remains ""
+                if ((two.size()==5 || two.size() >= 8) &&          	// if we have hh:mm or hh:mm:ss[.ffffff]
+                    !isAtLeastGivenLengthAndAllDigits(two, 6)) { 	// and it is not hhmmss
+                    three = "";    	                     		// do nothing, three remains ""
                 } else {
                     inp = two;
 
