b7d8e8429ce4b63532375cc455898107952ba55a
Dirk Eddelbuettel <edd@debian.org>
2016-12-19 19:50:51 -0600

    additional tests

--- tests/gh_issue_12.R
+++ tests/gh_issue_12.R
@@ -2,3 +2,6 @@ library(anytime)
 
 anydate("1969-01-01")    # crashed on Windows before tm_isdst diable for pre-epoch dates
 anydate("1971-02-03")    # fine anywhere
+
+stopifnot(anydate("1969-01-01") == as.Date("1969-01-01"))
+stopifnot(anydate("1971-02-03") == as.Date("1971-02-03"))
