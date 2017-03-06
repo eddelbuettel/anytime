136f8653c072daaa21690eb4689da5f69f79b199
Dirk Eddelbuettel <edd@debian.org>
2017-03-05 18:32:57 -0600

    special case for Europe/London

---
+++ tests/gh_issues_36_51.R
@@ -0,0 +1,13 @@
+library(anytime)
+
+if (anytime:::getTZ() != "Europe/London") {
+    cat("Note that you MUST use Europe/London to see anything here\n")
+}
+
+start <- anytime("1971-10-31 00:00:00")
+for (i in 0:25) {
+    now <- start + i*60*60
+    txt <- iso8601(now)
+    res <- iso8601(anytime(txt))
+    cat("Using ", now, " == ", txt, " parsed as ", res, "\n")
+}
