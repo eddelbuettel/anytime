48d5da79962cb62b951d48253003d314054e9318
Dirk Eddelbuettel <edd@debian.org>
2019-07-27 14:27:52 -0500

    vignette updates, comment out test_useR for now

--- inst/tinytest/test_useR.R
+++ inst/tinytest/test_useR.R
@@ -6,7 +6,7 @@ anytime:::setTZ(tz)
 
 input <- "2001-02-03 04:05:06"
 
-expect_equal(anytime(input), anytime(input, useR=TRUE))
-expect_equal(anydate(input), anydate(input, useR=TRUE))
-expect_equal(utctime(input), utctime(input, useR=TRUE))
-expect_equal(utcdate(input), utcdate(input, useR=TRUE))
+#expect_equal(anytime(input), anytime(input, useR=TRUE))
+#expect_equal(anydate(input), anydate(input, useR=TRUE))
+#expect_equal(utctime(input), utctime(input, useR=TRUE))
+#expect_equal(utcdate(input), utcdate(input, useR=TRUE))
