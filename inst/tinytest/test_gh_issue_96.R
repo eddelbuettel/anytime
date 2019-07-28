e3bde7fe053ccfb53b40acfe72c6e34d0f37d886
Dirk Eddelbuettel <edd@debian.org>
2019-07-28 09:36:43 -0500

    more polish to tests and vignette

--- inst/tinytest/test_gh_issue_96.R
+++ inst/tinytest/test_gh_issue_96.R
@@ -1,16 +1,20 @@
 
+tz <- "Japan"
+Sys.setenv("TZ"=tz)
+library(anytime)
+anytime:::setTZ(tz)
+
 isWindows <- Sys.info()[["sysname"]] == "Windows"
+isRelease <- length(unclass(utils::packageVersion("anytime"))[[1]]) == 3
 
 input <- "2019-07-05 09:37:58"
-
-Sys.setenv("TZ"="Japan")
-
-library(anytime)
 
 n1 <- as.numeric(anytime(input, useR = TRUE))
 n2 <- as.numeric(anytime(input))
 n3 <- as.numeric(as.POSIXct(input))
 
-if (!isWindows) expect_equal(n1, n2, tol=1e-5)
-expect_equal(n1, n3)
-if (!isWindows) expect_equal(n2, n3, tol=1e-5)
+if (!isWindows && !isRelease) {
+    expect_equal(n1, n2)
+    expect_equal(n1, n3)
+    expect_equal(n2, n3)
+}
--- inst/tinytest/test_useR.R
+++ inst/tinytest/test_useR.R
@@ -4,9 +4,14 @@ Sys.setenv("TZ"=tz)
 library(anytime)
 anytime:::setTZ(tz)
 
+isWindows <- Sys.info()[["sysname"]] == "Windows"
+isRelease <- length(unclass(utils::packageVersion("anytime"))[[1]]) == 3
+
 input <- "2001-02-03 04:05:06"
 
-#expect_equal(anytime(input), anytime(input, useR=TRUE))
-#expect_equal(anydate(input), anydate(input, useR=TRUE))
-#expect_equal(utctime(input), utctime(input, useR=TRUE))
-#expect_equal(utcdate(input), utcdate(input, useR=TRUE))
+if (!isWindows && !isRelease) {
+    expect_equal(anytime(input), anytime(input, useR=TRUE))
+    expect_equal(anydate(input), anydate(input, useR=TRUE))
+    expect_equal(utctime(input), utctime(input, useR=TRUE))
+    expect_equal(utcdate(input), utcdate(input, useR=TRUE))
+}
