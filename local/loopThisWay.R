f0319ad1905629b9aefb64bc6582a5505d26d201
Dirk Eddelbuettel <edd@debian.org>
2019-08-05 08:00:46 -0500

    new (local) test runner

---
+++ local/loopThisWay.R
@@ -0,0 +1,32 @@
+
+
+if (requireNamespace("tinytest", quietly=TRUE)) {
+
+    files <- list.files("../inst/tinytest", full.names = TRUE)
+
+    runTestFile <- function(file) {
+        tz_prior <- Sys.getenv("TZ")
+        Sys.sleep(0.1)
+        val <- tinytest::run_test_file(file)
+        Sys.sleep(0.1)
+        tz_post <- Sys.getenv("TZ")
+        c(tz_prior, tz_post)
+    }
+
+    runSerial <- function(files) {
+        for (file in files) {
+            res <- runTestFile(file)
+            cat(sprintf("TZ from %s to %s\n", res[1], res[2]))
+        }
+    }
+
+    runParallel <- function(files) {
+        rl <- parallel::mclapply(files, runTestFile, mc.cores=4)
+        res <- do.call(rbind, rl)
+        print(res)
+    }
+
+    runParallel(files)
+    runSerial(files)
+    runParallel(files)
+}
