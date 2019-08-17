74c59fac95f51a49cff86bf3f97c03ba00c298e5
Dirk Eddelbuettel <edd@debian.org>
2019-08-17 07:31:17 -0500

    another trial

--- inst/tinytest/test_all_formats.R
+++ inst/tinytest/test_all_formats.R
@@ -20,7 +20,7 @@ isRelease <- length(unclass(utils::packageVersion("anytime"))[[1]]) == 3
 ref <- anytime("2016-09-01 10:11:12")
 expect_equal(ref, anytime("2016-09-01 101112"))
 
-if (TRUE || isSolaris || isWindows || isRelease) exit_file("Skipping Solaris or Windows or Release")
+if (isSolaris || isWindows || isRelease) exit_file("Skipping Solaris or Windows or Release")
 
 options(digits.secs=6, width=70)
 
@@ -34,6 +34,7 @@ expect_equal(ref, format(anytime(c("20160901 10:11:12",   "20160901 10:11:12.345
 expect_equal(ref, format(anytime(c("09/01/2016 10:11:12", "09/01/2016 10:11:12.345678"))))
 expect_equal(ref, format(anytime(c("09-01-2016 10:11:12", "09-01-2016 10:11:12.345678"))))
 ## anytime(c("01.09.2016 10:11:12", "01.09.2016 10:11:12.345678")) # European format gets confused with m/d/y
+exit_file("Skip here now")
 
 expect_equal(ref, format(anytime(c("2016-Sep-01 10:11:12", "2016-Sep-01 10:11:12.345678"))))
 expect_equal(ref, format(anytime(c("2016/Sep/01 10:11:12", "2016/Sep/01 10:11:12.345678"))))
