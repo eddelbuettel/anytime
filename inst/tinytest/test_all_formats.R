eb5d7c4930b312614dc7146a2a46a040a8e6bf19
Dirk Eddelbuettel <edd@debian.org>
2019-08-18 07:22:19 -0500

    and one more small refactoring of test file

--- inst/tinytest/test_all_formats.R
+++ inst/tinytest/test_all_formats.R
@@ -52,6 +52,12 @@ expect_equal(ref, anytime("2016September01 10:11:12"))
 expect_equal(ref, anytime("September/01/2016 10:11:12"))
 expect_equal(ref, anytime("September-01-2016 10:11:12"))
 expect_equal(ref, anytime("01.September.2016 10:11:12"))
+expect_equal(ref, anytime("Thu Sep 01 10:11:12 2016"))
+expect_equal(ref, anytime("Thu 01 Sep 2016 10:11:12"))
+expect_equal(ref, anytime("2016-09-01T10:11:12 -05:00"))
+expect_equal(ref, anytime("2016-09-01T10:11:12 -0500"))
+expect_equal(ref, anytime("2016-09-01T10:11:12 CDT"))
+expect_equal(ref, anytime("Thu Sep 01 10:11:12 CDT 2016"))
 
 
 
@@ -82,36 +88,35 @@ expect_equal(ref, anytime("2016September01 10:11:12.345678"))
 expect_equal(ref, anytime("September/01/2016 10:11:12.345678"))
 expect_equal(ref, anytime("September-01-2016 10:11:12.345678"))
 expect_equal(ref, anytime("01.September.2016 10:11:12.345678"))
+expect_equal(ref, anytime("Thu Sep 01 10:11:12.345678 2016"))
+expect_equal(ref, anytime("Thu 01 Sep 2016 10:11:12.345678"))
+expect_equal(ref, anytime("2016-09-01T10:11:12.345678-05:00"))
+expect_equal(ref, anytime("2016-09-01T10:11:12.345678-0500"))
+expect_equal(ref, anytime("2016-09-01T10:11:12.345678 CDT"))
+expect_equal(ref, anytime("Thu Sep 01 10:11:12.345678 CDT 2016"))
 
 
-
-ref <- format(anytime(c("2016-09-01 10:11:12", "2016-09-01 10:11:12.345678")))
-expect_equal(ref, format(anytime(c("Thu Sep 01 10:11:12 2016", "Thu Sep 01 10:11:12.345678 2016"))))
-
-expect_equal(ref, format(anytime(c("Thu 01 Sep 2016 10:11:12", "Thu 01 Sep 2016 10:11:12.345678"))))
-
-expect_equal(ref, format(anytime(c("2016-09-01T10:11:12 -05:00", "2016-09-01T10:11:12.345678-05:00"))))
-expect_equal(ref, format(anytime(c("2016-09-01T10:11:12 -0500",  "2016-09-01T10:11:12.345678-0500"))))
-expect_equal(ref, format(anytime(c("2016-09-01T10:11:12 CDT",   "2016-09-01T10:11:12.345678 CDT"))))
-
-expect_equal(ref, format(anytime(c("Thu Sep 01 10:11:12 CDT 2016", "Thu Sep 01 10:11:12.345678 CDT 2016"))))
-
-
-                                        #ref <- format(rep(as.Date("2016-09-01"), 4))
-ref <- format(rep(anytime("2016-09-01"), 4))
-expect_equal(ref, format(anytime(c("2016-09-01", "20160901", "09/01/2016", "09-01-2016"))))
-
-expect_equal(ref, format(anytime(c("2016-Sep-01", "2016Sep01", "Sep/01/2016", "Sep-01-2016"))))
-
-expect_equal(ref[1:2], format(anytime(c("01SEP2016", "01-Sep-2016"))))
-
-expect_equal(ref, format(anytime(c("2016-September-01", "2016September01", "September/01/2016", "September-01-2016"))))
+ref <- anytime("2016-09-01")
+expect_equal(ref, anytime("2016-09-01"))
+expect_equal(ref, anytime("20160901"))
+expect_equal(ref, anytime("09/01/2016"))
+expect_equal(ref, anytime("09-01-2016"))
+expect_equal(ref, anytime("2016-Sep-01"))
+expect_equal(ref, anytime("2016Sep01"))
+expect_equal(ref, anytime("Sep/01/2016"))
+expect_equal(ref, anytime("Sep-01-2016"))
+expect_equal(ref, anytime("01SEP2016"))
+expect_equal(ref, anytime("01-Sep-2016"))
+expect_equal(ref, anytime("2016-September-01"))
+expect_equal(ref, anytime("2016September01"))
+expect_equal(ref, anytime("September/01/2016"))
+expect_equal(ref, anytime("September-01-2016"))
 
 
 ## special case of non-finite NA reals
 anytime(c(NA, NaN, Inf, as.numeric(as.POSIXct("2016-09-01 10:11:12"))))
 
-                                        #cat("\n")                               # special case of no separators
+##cat("\n")                               # special case of no separators
 anytime(c("20160911", "20160911 1011", "20160911 101112", "20160911 101112.345678"))
 
 cat("\n")
