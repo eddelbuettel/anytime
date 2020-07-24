26a3924faf9278437957625108daee5d64454a7c
Dirk Eddelbuettel <edd@debian.org>
2020-07-24 07:41:00 -0500

    release 0.3.8

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,12 +1,11 @@
 
-if (requireNamespace("tinytest", quietly=TRUE) &&
-    utils::packageVersion("tinytest") >= "1.0.0") {
+if (requireNamespace("tinytest", quietly=TRUE)) {
 
     ## Set a seed to make the test deterministic
     set.seed(42)
 
     ## there are several more granular ways to test files in a tinytest directory,
     ## see its package vignette; tests can also run once the package is installed
-    ## using the same command `test_package(pkgName)`, or by director or file
-    tinytest::test_package("anytime", ncpu=getOption("Ncpus", 1), side_effects=TRUE)
+    ## using the same command `test_package(pkgName)`, or by director, or by file
+    tinytest::test_package("anytime", side_effects=TRUE)
 }
