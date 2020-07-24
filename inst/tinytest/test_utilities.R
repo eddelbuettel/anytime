e93ebd5c47e000d35cc6c66e52bd5363dd0e6bfb
Dirk Eddelbuettel <edd@debian.org>
2020-07-24 07:55:55 -0500

    add a test one-liner

--- inst/tinytest/test_utilities.R
+++ inst/tinytest/test_utilities.R
@@ -26,5 +26,6 @@ expect_false(identical(f1, f2))
 #anytime:::setTZ("America/Chicago")
 #expect_equal(anytime:::format(s), "2016-09-01 10:11:12")
 
-## to trigger these two helpers
+## to trigger these two helpers: need two as it "exits early"
 expect_false(anytime:::.isDebian() && anytime:::.isUbuntu())
+expect_false(anytime:::.isUbuntu() && anytime:::.isDebian())
