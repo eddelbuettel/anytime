e8a1b3cc3effa6a10f03def20678665097c08f9b
Dirk Eddelbuettel <edd@debian.org>
2023-09-09 12:43:20 -0500

    Simplified test caller

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,11 +1,4 @@
 
 if (requireNamespace("tinytest", quietly=TRUE)) {
-
-    ## Set a seed to make the test deterministic
-    set.seed(42)
-
-    ## there are several more granular ways to test files in a tinytest directory,
-    ## see its package vignette; tests can also run once the package is installed
-    ## using the same command `test_package(pkgName)`, or by director, or by file
-    tinytest::test_package("anytime", side_effects=TRUE)
+    tinytest::test_package("anytime")
 }
